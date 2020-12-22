9eeb074505088af1b4d290b850eaa7b334de5028
Bernardo Hummes <hummescmpa@gmail.com>
2020-12-22 12:12:17 +0100

    separated non related functions in utils file

---
+++ include/utils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <map>
+#include <math.h>
+#include <iomanip>
+
+double euclidean_dist(std::pair<double,double> x1, std::pair<double,double> x2);
+
+double normalize(double src, double sup, double inf);
---
+++ src/utils.cpp
@@ -0,0 +1,10 @@
+#include "../include/utils.h"
+
+
+double euclidean_dist(std::pair<double,double> x1, std::pair<double,double> x2) {
+  return sqrt(pow(x1.first-x2.first,2) + pow((x1.second-x2.second),2));
+}
+
+double normalize(double src, double sup, double inf) {
+
+}
